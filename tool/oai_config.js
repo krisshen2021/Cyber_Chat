6924380e347197202aac042ba4c0e519ddde98b7
krisshen2021 <alphashen2000@gmail.com>
2024-09-09 09:46:42 +0800

    latest

--- tool/oai_config.js
+++ tool/oai_config.js
@@ -4,19 +4,4 @@ const oai_configs = [
         "api_key": "sk-714c9d913570489bac281d48caa48299",
         "model": "deepseek-coder"
     },
-    {
-        "api_base": "https://api.mistral.ai/v1",
-        "api_key": "eAmDTuAnQ4l0wmjOSWtgTXUQNFhItHj5",
-        "model": "mistral-large-latest"
-    },
-    {
-        "api_base": "https://openrouter.ai/api/v1",
-        "api_key": "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",
-        "model": "anthropic/claude-3.5-sonnet"
-    },
-    {
-        "api_base": "https://cors-anywhere.herokuapp.com/https://dashscope.aliyuncs.com/compatible-mode/v1",
-        "api_key": "sk-4698bba7f4ae4bda88b540f863804225",
-        "model": "qwen-turbo"
-    }
-];
+]
