a570dc8f459033a046df1c01e607852101b8e866
krisshen2021 <alphashen2000@gmail.com>
2024-09-14 17:22:03 +0800

    latest

--- tool/oai_config.js
+++ tool/oai_config.js
@@ -3,5 +3,10 @@ const oai_configs = [
         "api_base": "https://api.groq.com/openai/v1",
         "api_key": "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",
         "model": "gemma2-9b-it"
+    },
+    {
+        "api_base": "http://localhost:1234/v1",
+        "api_key": "null",
+        "model": "gemma-2-9b-it-q8_0"
     }
 ]
